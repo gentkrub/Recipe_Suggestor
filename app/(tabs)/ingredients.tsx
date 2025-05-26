43c5fc60d696cd2bc9828ad2711fee417dadc120
Gent <gentkrub@hotmail.com>
2025-05-26 12:24:16 +0700

    First commit

---
+++ app/(tabs)/ingredients.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ingredients = () => {
+  return (
+    <View>
+      <Text>ingredients</Text>
+    </View>
+  );
+};
+
+export default ingredients;
---
+++ app/(tabs)/searchtab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const searchtab = () => {
+  return (
+    <View>
+      <Text>searchtab</Text>
+    </View>
+  );
+};
+
+export default searchtab;
